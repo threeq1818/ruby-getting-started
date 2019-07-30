a17bf4119dc46d90239af94823412721af9a7b15
user <threeq1818@gmail.com>
2019-07-31 02:09:17 +0800

    generate controller Welcome index create a resource 'articles' generate controller Articles in ArticlesController create action 'new' and it's form in ArticlesController create action 'create' and coding save performance in ArticlesController create action 'show'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,24 @@
 class ArticlesController < ApplicationController
-  def new
 
+  def show
+    @article = Article.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    render plain: params[:article].inspect
+
+    @article = Article.new(params[article_params])
+    @article.save
+    redirect_to @article
+  end
+  
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
 end
